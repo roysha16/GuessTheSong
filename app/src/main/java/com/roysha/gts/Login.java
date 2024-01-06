f39cbc595b7e9ab12d548c1068b92826e2902e1e
amirco10 <amir.sharon@cymotive.com>
2024-01-06 09:43:41 +0200

    cleanup

--- app/src/main/java/com/roysha/gts/Login.java
+++ app/src/main/java/com/roysha/gts/Login.java
@@ -32,55 +32,6 @@ public class Login extends AppCompatActivity {
     Button buttonReg;
     Button buttonLogin;
     FirebaseAuth mAuth;
-/*
-    ///Roysha test
-    Map<String, Question> QuestionsList = new HashMap<>();
-
-    public void basicWriteDb() {
-
-        // Firebase Database.
-        FirebaseDatabase firebaseDatabase;
-        // Reference for Firebase.
-        DatabaseReference dbQReference;
-
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        dbQReference = firebaseDatabase.getReference("Questions");
-
-        Map<String, Question> Qs = new HashMap<>();
-        Qs.put("1", new  Question(2, "Question 1?", "Question 2?", "Question 3?", "Question 4?"));
-        Qs.put("2", new  Question(3, "Question 21?", "Question 22?", "Question 3?", "Question 4?"));
-        dbQReference.setValue(Qs);
-        Question Quest1 = new Question(4, "Question4 1?", "Question4 2?", "Qu4estion 3?", "Question 4?");
-        dbQReference.child("4").setValue(Quest1);
-    }
-    private void basicReadDb() {
-
-        FirebaseDatabase firebaseDatabase;
-        DatabaseReference databaseReference;
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference("Questions");
-        databaseReference.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
-                Question question = dataSnapshot.getValue(Question.class);
-                if(previousChildName == null)
-                    QuestionsList.clear();
-                QuestionsList.put(dataSnapshot.getKey(),question);
-            }
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
-            }
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-            }
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-            }
-        });
-    }*/
 
     public void LoginSuccess() {
         FirebaseUser currentUser = mAuth.getCurrentUser();
